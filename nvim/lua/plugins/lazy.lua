1c2a587835c4d0c6b504198c78c44a59df21f9a1
Andrey Baghdasaryan <andrey.baghdasaryan@nordicsemi.no>
2024-06-05 19:01:38 +0200

    copilotchat enabled

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -67,6 +67,20 @@ require('lazy').setup({
   },
 
   'github/copilot.vim',
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   'folke/zen-mode.nvim',
   'tpope/vim-obsession',
   -- Tree
